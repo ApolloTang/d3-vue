87ecaf3aeaabb2d85901443f3a3203c27fc592cf
Apollo Tang <tang.apollo@gmail.com>
2018-04-04 17:40:19 -0400

    use computed

--- src/studies-basic/02-vue-handle-svg-attr/main.js
+++ src/studies-basic/02-vue-handle-svg-attr/main.js
@@ -3,56 +3,51 @@ const local = window.local = {local:'local'}
 
 export default main;
 
-
-const data = [99, 71, 78, 25, 36, 92];
-
 const vm = window.vm = new Vue({
   el: '#app',
 
   template: `
   <svg width="200" height="100">
     <g style="transform: translate(0, 10px)">
-      <path :d="line" />
+      <path :d="path" />
     </g>
   </svg>`,
 
   data() {
     return {
-      'line': ''
+      data : [],
     }
   },
 
-  mounted() {
-    this.calculatePath();
-  },
-
-  methods: {
-
+  computed: {
     getScales(){
-      const svg = d3.select(this.$el)
-        .append('svg')
-        .attr('width', 200)
-        .attr('height', 100)
-        .append('g')
-        .attr('transform', 'translate(0,10)')
-
       const xScale = d3.scaleLinear().range([0, 200])
       const yScale = d3.scaleLinear().range([100, 0])
 
-      xScale.domain(d3.extent(data, (d,i)=>i))
-      yScale.domain([0, d3.max(data)])
+      xScale.domain(d3.extent(this.data, (d,i)=>i))
+      yScale.domain([0, d3.max(this.data)])
 
       return {xScale, yScale}
     },
 
-    calculatePath() {
-      const {xScale, yScale} = this.getScales()
+    path() {
+      const {xScale, yScale} = this.getScales
       const path = d3.line()
         .x( (d,i)=>xScale(i) )
         .y( (d,i)=>yScale(d) )
-      this.line = path(data)
+      return path(this.data)
     }
   },
 
 })
+
+
+setInterval( ()=>{
+  const tmpData = []
+  let i = 0
+  while (++i <= 10) {
+   tmpData.push(chance.natural({min:0, max:100}))
+  }
+  vm.data = tmpData
+} , 1000)
 
